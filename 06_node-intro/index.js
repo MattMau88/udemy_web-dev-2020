1ed7ec5c981691b1ef5aab8789c90c3a3377b5c6
Matthias Mau <matthias.mau@outlook.com>
2020-08-16 15:09:43 +0200

    commit

---
+++ 06_node-intro/index.js
@@ -0,0 +1,7 @@
+// const fs = require('fs');
+// fs.copyFileSync("file1.txt", "file2.txt");
+
+var superheroes = require('superheroes');
+
+var randomSH = superheroes.random();
+console.log(randomSH);
