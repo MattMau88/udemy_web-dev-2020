c02c6293e20d52ed7d0c6c77c9def5e96f9f88c9
Matthias Mau <matthias.mau@outlook.com>
2020-08-29 10:00:19 +0300

    updated project templyte + instructions

--- 00_templates/app.js
+++ 00_templates/app.js
@@ -2,14 +2,15 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 // const https = require('https');
 // const request = require("request");
 
 const app = express();
 const port = 3000;
 
-// app.set('view engine', 'ejs');
-// app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
