fae69a18cca4ca8ee7e6f940f3eefb80085b36b8
Matthias Mau <matthias.mau@outlook.com>
2020-08-30 18:52:09 +0300

    added further CRUD operation to code

--- 13_MongoDB-with-Mongoose/app.js
+++ 13_MongoDB-with-Mongoose/app.js
@@ -3,37 +3,185 @@ const mongoose = require('mongoose');
 // Connection URL
 mongoose.connect('mongodb://localhost:27017/peopleDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
-//create new schema
+// //create new schema
+// const peopleSchema = new mongoose.Schema ({
+//   name: {
+//     type: String,
+//     required: [true, "A person must have a name"]
+//   },
+//   age: {
+//     type: Number,
+//     min: 1,
+//     max: 99
+//   }
+//   favoriteFruit: fruitsSchema
+// });
+//
+// //use schema to create a Mongoose model
+// const Person = mongoose.model("Person", peopleSchema);
+//
+// // create one new document
+// const person = new Person ({
+//   name: "Jimmy",
+//   age: 99
+// });
+
+// person.save();
+
+// creating multiple new documents/entries
+// const elina = new Person({
+//   name: "Elina",
+//   age: 52
+// })
+//
+// const mers = new Person({
+//   name: "Mers",
+//   age: 26
+// })
+
+// Person.insertMany([elina, mers], function(err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successfully save multiple new entries to people")
+//   }
+// });
+
+//finding entries
+// Person.find(function(err, people){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//
+//     //too close the connection once done running an operation
+//     mongoose.connection.close();
+//
+//     people.forEach((person) => {
+//       console.log(person.name);
+//     });
+//   }
+// });
+
+//updating documents
+// Person.updateOne({_id: "5f4bae17157619460622463b"},
+//   {
+//     name: "Ryan",
+//     age: 40
+//   },
+//   function(err){
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("Successfully updated document");
+//     }
+//   }
+// );
+
+//deleting documents
+// Person.deleteOne({ _id: "5f4bba3003029f4ae1d378c3" }, function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successfully deleted document");
+//   }
+// });
+
+//RELATIONSHIPS
+// add second schema to peopleDB
+const fruitsSchema = new mongoose.Schema ({
+  name: {
+    type: String,
+    required: [true, "A fruit must have a name"]
+  },
+  score: {
+    type: Number,
+    min: 1,
+    max: 10
+  },
+  review: String
+});
+
+// use schema to create a Mongoose model
+const Fruit = mongoose.model("Fruit", fruitsSchema);
+
+const pineapple = new Fruit({
+  name: "Pineapple",
+  score: 5,
+  review: "Weird taste"
+})
+
+//pineapple.save();
+//
+// //--pasting PeopleSchema from above here to again to create relationship after initiating fruitsSchema
+// //create new schema
 const peopleSchema = new mongoose.Schema ({
-  name: String,
-  age: Number
+  name: {
+    type: String,
+    required: [true, "A person must have a name"]
+  },
+  age: {
+    type: Number,
+    min: 1,
+    max: 99
+  },
+  favoriteFruit: fruitsSchema
 });
 
 //use schema to create a Mongoose model
 const Person = mongoose.model("Person", peopleSchema);
 
-// create new documents
-const person = new Person ({
-  name: "John",
-  age: 26
-});
+// // create one new document
+// const person = new Person ({
+//   name: "Amy",
+//   age: 99,
+//   favoriteFruit: pineapple
+// });
+//
+// person.save();
 
-//person.save();
 
-const dimi = new Person({
-  name: "Dimi",
-  age: 23
+// const apple = new Fruit({
+//   name: "Apple",
+//   score: 8,
+//   review: "Great fruit"
+// })
+//
+// const orange = new Fruit({
+//   name: "Orange",
+//   score: 6,
+//   review: "Kinda sour"
+// })
+//
+// const banana = new Fruit({
+//   name: "Banana",
+//   score: 9,
+//   review: "Very nutricious"
+// })
+//
+// Fruit.insertMany([apple, orange, banana], function(err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successfully save multiple new entries to fruits")
+//   }
+// });
+
+//ADDTIONAL, repetitive excercise
+const mango = new Fruit({
+  name: "Mango",
+  score: "4",
+  review: "Decent fruit"
 })
 
-const matt = new Person({
-  name: "Matt",
-  age: 32
-})
+mango.save();
 
-Person.insertMany([dimi, matt], function(err) {
+Person.updateOne({name: "Mers"}, {favoriteFruit: mango}, function(err){
   if (err) {
     console.log(err);
   } else {
-    console.log("Successfully save multiple new entries to people")
+    //too close the connection once done running an operation
+    mongoose.connection.close();
+
+    console.log("Added favortite fruit to Mers");
   }
 });
